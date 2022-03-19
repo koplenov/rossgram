4f6c7fac5bce453a1614efba63473104dbab1eee
Rrezakk <kirill.reznikov.2011107@gmail.com>
2022-03-19 12:48:52 +0300

    Initial

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using rossgram;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<UserDataDBOptions>(builder.Configuration.GetSection("Connection"));
+builder.Services.AddCors();
+//builder.Services.AddSingleton<>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.Run();
---
+++ UserDataDBOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace rossgram
+{
+    public class UserDataDBOptions
+    {
+        public string CollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
