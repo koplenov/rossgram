50f19f6602ebdb936e9864a2eb19395eeab9e2dd
Rrezakk <kirill.reznikov.2011107@gmail.com>
2022-03-19 15:33:28 +0300

    dkjf

---
+++ Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace rossgram
+{
+    public class UserModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public List<string> Images { get; set; }
+
+        public UserModel() { }
+
+        public UserModel(string login, string pass, string role)
+        {
+            this.Images = new List<string>();
+            this.Login = login;
+            this.Password = pass;
+        }
+
+    }
+}
--- UserDataService.cs
+++ UserDataService.cs
@@ -13,9 +13,9 @@ namespace rossgram
             users = MongoClient.GetDatabase(settings.Value.DatabaseName)
                 .GetCollection<UserModel>(settings.Value.CollectionName);
         }
-        public UserModel Getuser(string name) => users.Find(u => u.Name == name).FirstOrDefault();
+        public UserModel Getuser(string login) => users.Find(u => u.Login == login).FirstOrDefault();
         public void AddUser(UserModel user) => users.InsertOne(user);
-        public void DeleteUser(string name) => users.DeleteOne(name);
-        public void UpdateUser(string name, UserModel user) => users.ReplaceOne(u => u.Name == name,user);
+        public void DeleteUser(string login) => users.DeleteOne(u=>u.Login== login);
+        public void UpdateUser(string login, UserModel user) => users.ReplaceOne(u => u.Login == login, user);
     }
 }
